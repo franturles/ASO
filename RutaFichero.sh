e5d3f28fced8bc4d01cbb94daf661ce4949147ab
kikofran35@hotmail.com <franturles>
2022-02-01 22:14:06 +0100

    Depurar while

--- RutaFichero.sh
+++ RutaFichero.sh
@@ -3,27 +3,61 @@ clear
 if [ $# -ge 1 ]
 then
 	for var in $*
-	do
-	localizacion=$(find /home -name $var)
-        if [ -f "$localizacion" ]
-	then
-	echo "Eso es un fichero"
-	find /home -name $var
-	ls -l $localizacion
-	cat $localizacion
-	sleep 2
-	elif [ -d "$localizacion" ]
-	then
-	echo "Eso es un directorio"
-	find /home -name /$var 2> /dev/null
-	tree $localizacion
-	ls -l $localizacion
-	sleep 2
-	fi
+		do
+		localizacion=$(find /home -name $var)
+		echo "Selecciona una de estas opciones:"
+		echo "1. Listar archivo o directorio"
+		echo "2. Ver el contenido de archivo o directorio"
+		echo "3. Ver permisos de archivo o directorio"
+		read op
+		while true; do
+		case $op in
+			1) echo "Listar"
+			if [ -f "$localizacion" ]
+        			then
+                		echo "Eso es un fichero"
+                		find /home -name $var
+                		sleep 2
+        		elif [ -d "$localizacion" ]
+        			then
+                		echo "Eso es un directorio"
+                		find /home -name /$var 2> /dev/null
+        		fi
+				;;
+
+			2) echo "Contenido"
+			if [ -f "$localizacion" ]
+        			then
+                		echo "Eso es un fichero"
+                		cat $localizacion
+                		sleep 2
+        		elif [ -d "$localizacion" ]
+        			then
+                		echo "Eso es un directorio"
+                		tree $localizacion
+                		sleep 2
+			fi
+				;;
+
+			3)
+			echo "Permisos"
+			if [ -f "$localizacion" ]
+        			then
+                		echo "Eso es un fichero"
+                		ls -l $localizacion
+                		sleep 2
+        		elif [ -d "$localizacion" ]
+        			then
+                		echo "Eso es un directorio"
+                		ls -l $localizacion
+                		sleep 2
+        		fi
+				;;
+		esac
+		shift
+		done
 	done
-
 else
-clear
-echo "Tienes que poner como minimo 1 parametro"
+	clear
+	echo "Tienes que poner como minimo 1 parametro"
 fi
-
