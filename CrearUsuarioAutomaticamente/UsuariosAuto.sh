f0eb69d3570a5001511c11d105905b19538759d5
kikofran35@hotmail.com <franturles>
2022-02-02 22:05:54 +0100

    Problema con while

--- CrearUsuarioAutomaticamente/UsuariosAuto.sh
+++ CrearUsuarioAutomaticamente/UsuariosAuto.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
+clear
 if [ $# -ge 1 ]
 	then
 	grupo=$(cut -f1 -d ',' $1)
-	Numero=$(cut -f2 -d ',' $1)
-		for var in $grupo
-		do
+	NumeroUS=$(cut -f2 -d ',' $1)
+	for var in $grupo
+	do
 		groupdel $var 2> /dev/null
-		groupadd $var
+		groupadd $var 2> /dev/null
 		echo "Se ha creado el grupo $var"
-		for var2 in $Numero
+		echo "Creando Usuarios para el grupo $var"
+		sleep 3
+		for var2 in $NumeroUS
 		do
-			number=0
-			while [ "$number" -lt "$var2" ]
+			number=1
+			while [ "$number" -le "$var2" ]
 			do
-				NombreUsuario=$($var-$number)
-				echo "$NombreUsuario"
+				#NombreUsuario=$($var'-'$number)
+				userdel -r $var-$number 2> /dev/null
+				#useradd $var-$number -m -d /home/$var-$number -G $var
+				echo "Se ha creado el usuario $var-$number"
 				number=$(($number+1))
 			done
-
+			sleep 2
 		done
 	done
 
