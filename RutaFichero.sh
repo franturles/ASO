b4a2d9862ed4db4369874aa1474e0e6e7a187d47
kikofran35@hotmai.com <franturles>
2022-02-02 13:24:09 +0100

    Terminado

--- RutaFichero.sh
+++ RutaFichero.sh
@@ -2,62 +2,80 @@
 clear
 if [ $# -ge 1 ]
 then
-	for var in $*
-		do
-		localizacion=$(find /home -name $var)
-		echo "Selecciona una de estas opciones:"
-		echo "1. Listar archivo o directorio"
-		echo "2. Ver el contenido de archivo o directorio"
-		echo "3. Ver permisos de archivo o directorio"
-		read op
-		while true; do
-		case $op in
-			1) echo "Listar"
-			if [ -f "$localizacion" ]
-        			then
-                		echo "Eso es un fichero"
-                		find /home -name $var
-                		sleep 2
-        		elif [ -d "$localizacion" ]
-        			then
-                		echo "Eso es un directorio"
-                		find /home -name /$var 2> /dev/null
-        		fi
-				;;
+        for var in $*
+                do
+                localizacion=$(find /home -name $var)
+                echo "Selecciona una de estas opciones para $var:"
+                echo "1. Ver ruta de archivo o directorio"
+                echo "2. Ver el contenido de archivo o directorio"
+                echo "3. Ver permisos de archivo o directorio"
+                read op
+                while true; do
+                clear
+                case $op in
+                        1) echo "Ruta"
+                        if [ -f "$localizacion" ]
+                                then
+                                echo "$var es un fichero"
+                                find / -name $var 2> /dev/null
+                                sleep 2
+                                clear
+                                break
+                        elif [ -d "$localizacion" ]
+                                then
+                                echo "$var es un directorio"
+                                find / -name $var 2> /dev/null
+                                clear
+                                break
+                        fi
+                                ;;
 
-			2) echo "Contenido"
-			if [ -f "$localizacion" ]
-        			then
-                		echo "Eso es un fichero"
-                		cat $localizacion
-                		sleep 2
-        		elif [ -d "$localizacion" ]
-        			then
-                		echo "Eso es un directorio"
-                		tree $localizacion
-                		sleep 2
-			fi
-				;;
+                        2) echo "Contenido"
+                        if [ -f "$localizacion" ]
+                                then
+                                echo "$var es un fichero"
+                                cat $localizacion
+                                sleep 2
+                                clear
+                                break
+                        elif [ -d "$localizacion" ]
+                                then
+                                echo "$var es un directorio"
+                                tree $localizacion
+                                sleep 2
+                                clear
+                                break
+                        fi
+                                ;;
 
-			3)
-			echo "Permisos"
-			if [ -f "$localizacion" ]
-        			then
-                		echo "Eso es un fichero"
-                		ls -l $localizacion
-                		sleep 2
-        		elif [ -d "$localizacion" ]
-        			then
-                		echo "Eso es un directorio"
-                		ls -l $localizacion
-                		sleep 2
-        		fi
-				;;
-		esac
-		shift
-		done
-	done
+                        3)
+                        echo "Permisos"
+                        if [ -f "$localizacion" ]
+                                then
+                                echo "$var es un fichero"
+                                ls -l $localizacion
+                                sleep 2
+                                clear
+                                break
+                        elif [ -d "$localizacion" ]
+                                then
+                                echo "$var es un directorio"
+                                ls -l $localizacion
+                                sleep 2
+                                clear
+                                break
+                        fi
+                                ;;
+                esac
+                done
+        done
 else
-	clear
-	echo "Tienes que poner como minimo 1 parametro"
+        clear
+        echo "Tienes que poner como minimo 1 parametro"
 fi
+
+
+
+
+
+
