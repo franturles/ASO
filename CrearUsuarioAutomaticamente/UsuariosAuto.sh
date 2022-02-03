e001fe028de6406e130c9f5af4257288f9792728
kikofran35@hotmai.com <franturles>
2022-02-03 16:42:13 +0100

    Terminado

--- CrearUsuarioAutomaticamente/UsuariosAuto.sh
+++ CrearUsuarioAutomaticamente/UsuariosAuto.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 clear
+archivo=$(cat $1)
 if [ $# -ge 1 ]
-	then
-	grupo=$(cut -f1 -d ',' $1)
-	NumeroUS=$(cut -f2 -d ',' $1)
-	for var in $grupo
-	do
-		groupdel $var 2> /dev/null
-		groupadd $var 2> /dev/null
-		echo "Se ha creado el grupo $var"
-		echo "Creando Usuarios para el grupo $var"
-		sleep 3
-		for var2 in $NumeroUS
-		do
-			number=1
-			while [ "$number" -le "$var2" ]
-			do
-				#NombreUsuario=$($var'-'$number)
-				userdel -r $var-$number 2> /dev/null
-				#useradd $var-$number -m -d /home/$var-$number -G $var
-				echo "Se ha creado el usuario $var-$number"
-				number=$(($number+1))
-			done
-			sleep 2
-		done
-	done
+        then
+         for var in $archivo
+         do
+                grupo=$(echo $var | cut -d, -f1)
+                usuarios=$(echo $var | cut -d, -f2)
+                groupdel $grupo 2> /dev/null
+                groupadd $grupo 2> /dev/null
+                echo "Se ha creado el grupo $grupo"
+                echo "Creando Usuarios para el grupo $grupo"
+                sleep 3
+                for var2 in $usuarios
+                do
+                        number=1
+                        while [ "$number" -le "$var2" ]
+                        do
+                                #NombreUsuario=$($var'-'$number)
+                                userdel -r $grupo-$number 2> /dev/null
+                                useradd $grupo-$number -m -d /home/$grupo-$number -G $grupo
+                                echo "Se ha creado el usuario $grupo-$number"
+                                number=$(($number+1))
+                        done
+                        break
+                        sleep 2
+                done
+        done
 
 else
-	echo "Debes pasar un fichero"
+        echo "Debes pasar un fichero"
 fi
+
