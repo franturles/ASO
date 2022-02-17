512379e9343a29fc9ce19c31c4c519eab6c661a2
kikofran35@hotmai.com <franturles>
2022-02-17 17:48:45 +0100

    Añadidos comentarios

--- Informe.sh
+++ Informe.sh
@@ -4,6 +4,23 @@ then
 	echo "No hay parametros"
 	sleep 2
 	echo "Procediendo a la recopilacion de todos los usuarios"
+	Usuarios=$(getent passwd {1000..60000} | cut -d: -f1 | grep $2 -o | uniq)
+	for Usuario in $Usuarios
+	do
+		echo "#################################################"
+		echo "Usuario:$Usuario"
+		echo "Nº Ficheros de los que es propietario:"
+		echo "Nº Ficheros que puede modificar:"
+		echo "Nº Ficheros abiertos:"
+		echo "Ficheros más antiguo del usuario:"
+		echo "Fichero más recientemente modificado:"
+		echo "Tamaño fichero más pequeño:"
+		echo "Tamaño medio de fichero:"
+		echo "Tamaño fichero más grande:"
+		echo "Tipo de fichero más usual:"
+		echo "#################################################"
+		read pause
+	done
 elif [ $# > 2 ]
 then
 	if [ $1 = -u ]
