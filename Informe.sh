534bec956a4038ac4227c102cb975962f6359f81
kikofran35@hotmail.com <franturles>
2022-02-20 21:17:51 +0100

    Problemas con una variable y tamaño fichero medio

--- Informe.sh
+++ Informe.sh
@@ -4,17 +4,25 @@ then
 	echo "No hay parametros"
 	sleep 2
 	echo "Procediendo a la recopilacion de todos los usuarios"
-	Usuarios=$(getent passwd {1000..60000} | cut -d: -f1 | grep $2 -o | uniq)
-	for Usuario in $Usuarios
+	ListadoUsuarios=$(getent passwd {1000..60000} | cut -d: -f1,6)
+	for Usuarios in $ListadoUsuarios
 	do
+		Usuario=$(echo $Usuarios | cut -d: -f1)
+		HomeUsuarios=$(echo $Usuarios| cut -d: -f2)
+		NumeroFicherosPro=$(find $HomeUsuarios -user $Usuario | wc -l)
+		NumeroFicherosMod=$(find $HomeUsuarios -user $Usuario -perm -u+w | wc -l)
+		#NumeroFicherosAb=$( ps -u $Usuario | grep nano | wc -l)
+		FicheroAntiguo=$(ls $HomeUsuarios -tr -R | head -n2 | tail -n 1)
+		FicheroMasReciente=$(ls $HomeUsuarios -t -R | head -n2 | tail -n 1)
+		FicheroMasPequeño=$(ls -s -R $HomeUsuarios | head -n3 | tail -n1)
 		echo "#################################################"
 		echo "Usuario:$Usuario"
-		echo "Nº Ficheros de los que es propietario:"
-		echo "Nº Ficheros que puede modificar:"
+		echo "Nº Ficheros de los que es propietario:$NumeroFicherosPro"
+		echo "Nº Ficheros que puede modificar:$NumeroFicherosMod"
 		echo "Nº Ficheros abiertos:"
-		echo "Ficheros más antiguo del usuario:"
-		echo "Fichero más recientemente modificado:"
-		echo "Tamaño fichero más pequeño:"
+		echo "Ficheros más antiguo del usuario: $FicheroAntiguo "
+		echo "Fichero más recientemente modificado: $FicheroMasReciente"
+		echo "Tamaño fichero más pequeño: $FicheroMasPequeño"
 		echo "Tamaño medio de fichero:"
 		echo "Tamaño fichero más grande:"
 		echo "Tipo de fichero más usual:"
@@ -25,12 +33,12 @@ elif [ $# > 2 ]
 then
 	if [ $1 = -u ]
 	then
-		UsuarioEs=$(getent passwd {1000..60000} | cut -d: -f1 | grep $2 -o | uniq)
+		UsuarioEs=$(getent passwd {1000..60000} | cut -d: -f1 | grep -o "$2" | uniq)
 		if [ -z $UsuarioEs ]
 		then
 			echo "No existe ese usuario"
 		else
-			NumFicherosProEs=$(ls -l /home/$UsuarioEs | cut -d" " -f2)
+			NumeroFicherosPro=$(find /home/usuario -user usuario | wc -l)
 			echo "Existe el usuario $UsuarioEs"
 			echo "#################################################"
 	                echo "Usuario:$UsuarioEs"
