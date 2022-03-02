e98f5f5639db6719e7d7b20fc468f1b61062415c
kikofran35@hotmail.com <franturles>
2022-03-02 13:22:37 +0100

    añadido las ultimas variables

--- Informe.sh
+++ Informe.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+clear
+echo "Espere por favor..."
 if [ $# = 0 ]
 then
 	echo "No hay parametros"
@@ -9,13 +11,15 @@ then
 	do
 		Usuario=$(echo $Usuarios | cut -d: -f1)
 		HomeUsuarios=$(echo $Usuarios| cut -d: -f2)
-		NumeroFicherosPro=$(find $HomeUsuarios -user $Usuario | wc -l)
-		NumeroFicherosMod=$(find $HomeUsuarios -user $Usuario -perm -u+w | wc -l)
+		NumeroFicherosPro=$(find $HomeUsuarios -type f -user $Usuario | wc -l)
+		NumeroFicherosMod=$(find $HomeUsuarios -type f -user $Usuario -perm -u+w | wc -l)
 		NumeroFicherosAb=$( lsof -u $Usuario 2> /dev/null | wc -l)
 		FicheroAntiguo=$(ls $HomeUsuarios -tr -R | head -n2 | tail -n 1)
 		FicheroMasReciente=$(ls $HomeUsuarios -t -R | head -n2 | tail -n 1)
-		FicheroMasPequeño=$(ls -s -R $HomeUsuarios | head -n3 | tail -n1)
-		FicheroMasPesado=$(du $HomeUsuarios | sort -n -r | head -n3 )
+		FicheroMasPeque=$(find / -user $Usuario -type f -printf "%s %f\0" 2>/dev/null | sort -z -n | head -z -n 1 | cut -z -d ' ' -f 2 | tr '\0' '\n')
+		FicheroMasPesado=$(find / -user $Usuario -type f -printf "%s %f\0" 2>/dev/null | sort -z -n | tail -z -n 1 | cut -z -d ' ' -f 1 | tr '\0' '\n')
+		FicheroMedio=$(find / -user $Usuario -ls 2>/dev/null | awk '{sum += $7; n++;} END {print sum/n;}')
+		#FicheroUsual=$() Sin solucionar
 		echo "#################################################"
 		echo "Usuario:$Usuario"
 		echo "Nº Ficheros de los que es propietario:$NumeroFicherosPro"
@@ -23,10 +27,10 @@ then
 		echo "Nº Ficheros abiertos:$NumeroFicherosAb"
 		echo "Ficheros más antiguo del usuario: $FicheroAntiguo "
 		echo "Fichero más recientemente modificado: $FicheroMasReciente"
-		echo "Tamaño fichero más pequeño: $FicheroMasPequeño"
-		echo "Tamaño medio de fichero:"
+		echo "Tamaño fichero más pequeño: $FicheroMasPeque"
+		echo "Tamaño medio de fichero:$FicheroMedio"
 		echo "Tamaño fichero más grande:$FicheroMasPesado"
-		echo "Tipo de fichero más usual:"
+		echo "Tipo de fichero más usual: Sin concretar"
 		echo "#################################################"
 		read pause
 	done
@@ -36,14 +40,14 @@ then
 	then
 		UsuarioEs=$(getent passwd {1000..60000} | cut -d: -f1 | grep -o "$2" | uniq)
 		HomeUsuariosEs=$(getent passwd {1000..60000} | grep "$2" | cut -d: -f6)
-                NumeroFicherosPro=$(find $HomeUsuariosEs -user $UsuarioEs | wc -l)
-                NumeroFicherosMod=$(find $HomeUsuariosEs -user $UsuarioEs -perm -u+w | wc -l)
-                NumeroFicherosAb=$( lsof -u $UsuarioEs | wc -l)
+                NumeroFicherosPro=$(find $HomeUsuariosEs -type f -user $UsuarioEs | wc -l)
+                NumeroFicherosMod=$(find $HomeUsuariosEs -type f -user $UsuarioEs -perm -u+w | wc -l)
+                NumeroFicherosAb=$( lsof -u $UsuarioEs 2> /dev/null | wc -l)
                 FicheroAntiguo=$(ls $HomeUsuariosEs -tr -R | head -n2 | tail -n 1)
                 FicheroMasReciente=$(ls $HomeUsuariosEs -t -R | head -n2 | tail -n 1)
-                FicheroMasPequeño=$(ls -s -R $HomeUsuariosEs | head -n3 | tail -n1)
-                FicheroMasPesado=$(du $HomeUsuariosEs | sort -n -r | head -n3 )
-
+		FicheroMasPeque=$(find / -user $UsuarioEs -type f -printf "%s %f\0" 2>/dev/null | sort -z -n | head -z -n 1 | cut -z -d ' ' -f 2 | tr '\0' '\n')
+                FicheroMasPesado=$(find / -user $UsuarioEs -type f -printf "%s %f\0" 2>/dev/null | sort -z -n | tail -z -n 1 | cut -z -d ' ' -f 1 | tr '\0' '\n')
+                FicheroMedio=$(find / -user $UsuarioEs -ls 2>/dev/null | awk '{sum += $7; n++;} END {print sum/n;}')
 		if [ -z $UsuarioEs ]
 		then
 			echo "No existe ese usuario"
@@ -52,15 +56,15 @@ then
 			echo "Existe el usuario $UsuarioEs"
 			echo "#################################################"
 	                echo "Usuario:$UsuarioEs"
-	                echo "Nº Ficheros de los que es propietario:$NumFicherosProEs "
+	                echo "Nº Ficheros de los que es propietario:$NumeroFicherosPro"
 	                echo "Nº Ficheros que puede modificar:$NumeroFicherosMod"
 	                echo "Nº Ficheros abiertos:$NumeroFicherosAb "
 	                echo "Ficheros más antiguo del usuario:$FicheroAntiguo"
 	                echo "Fichero más recientemente modificado:$FicheroMasReciente"
-	                echo "Tamaño fichero más pequeño:$FicheroMasPequeño"
-	                echo "Tamaño medio de fichero:"
+	                echo "Tamaño fichero más pequeño:$FicheroMasPeque"
+	                echo "Tamaño medio de fichero:$FicheroMedio"
 	                echo "Tamaño fichero más grande:$FicheroMasPesado"
-	                echo "Tipo de fichero más usual:"
+	                echo "Tipo de fichero más usual: Sin concretar"
 	                echo "#################################################"
 	                read pause
 
@@ -68,28 +72,29 @@ then
 	elif [ $1 = -c ]
 	then
 		UsuariosConectados=$(who | cut -d" " -f1)
-		for UsuarioConectado in $UsuariosConectados
+		for UsuarioCo in $UsuariosConectados
 		do
-                	HomeUsuariosCo=$(getent passwd {1000..60000} | grep "$UsuarioConectado" | cut -d: -f6)
-                	NumeroFicherosPro=$(find $HomeUsuariosCo -user $UsuarioCo | wc -l)
-               		NumeroFicherosMod=$(find $HomeUsuariosCo -user $UsuarioCo -perm -u+w | wc -l)
-               		NumeroFicherosAb=$( lsof -u $UsuarioCo | wc -l)
+                	HomeUsuariosCo=$(getent passwd {1000..60000} | grep "$UsuarioCo" | cut -d: -f6)
+                	NumeroFicherosPro=$(find $HomeUsuariosCo -type f -user $UsuarioCo | wc -l)
+               		NumeroFicherosMod=$(find $HomeUsuariosCo -type f -user $UsuarioCo -perm -u+w | wc -l)
+               		NumeroFicherosAb=$( lsof -u $UsuarioCo 2> /dev/null | wc -l)
                 	FicheroAntiguo=$(ls $HomeUsuariosCo -tr -R | head -n2 | tail -n 1)
                 	FicheroMasReciente=$(ls $HomeUsuariosCo -t -R | head -n2 | tail -n 1)
-        	        FicheroMasPequeño=$(ls -s -R $HomeUsuariosCo | head -n3 | tail -n 1)
-	                FicheroMasPesado=$(du $HomeUsuariosCo | sort -n -r | head -n3 )
+			FicheroMasPeque=$(find / -user $UsuarioCo -type f -printf "%s %f\0" 2>/dev/null | sort -z -n | head -z -n 1 | cut -z -d ' ' -f 2 | tr '\0' '\n')
+                	FicheroMasPesado=$(find / -user $UsuarioCo -type f -printf "%s %f\0" 2>/dev/null | sort -z -n | tail -z -n 1 | cut -z -d ' ' -f 1 | tr '\0' '\n')
+                	FicheroMedio=$(find / -user $UsuarioCo -ls 2>/dev/null | awk '{sum += $7; n++;} END {print sum/n;}')
 
 			echo "#################################################"
-                        echo "Usuario:$UsuarioConectado"
+                        echo "Usuario:$UsuarioCo"
                         echo "Nº Ficheros de los que es propietario:$NumeroFicherosPro"
                         echo "Nº Ficheros que puede modificar:$NumeroFicherosMod"
                         echo "Nº Ficheros abiertos:$NumeroFicherosAb "
                         echo "Ficheros más antiguo del usuario:$FicheroAntiguo"
                         echo "Fichero más recientemente modificado:$FicheroMasReciente"
-                        echo "Tamaño fichero más pequeño:$FicheroMasPequeño"
-                        echo "Tamaño medio de fichero:"
+                        echo "Tamaño fichero más pequeño:$FicheroMasPeque"
+                        echo "Tamaño medio de fichero:$FicheroMedio"
                         echo "Tamaño fichero más grande:$FicheroMasPesado"
-                        echo "Tipo de fichero más usual:"
+                        echo "Tipo de fichero más usual: Sin concretar"
                         echo "#################################################"
                         read pause
 
